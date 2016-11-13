587f1eb4f6ab96bc0054079f6360e3d54dbe68c2
root <root@NBELM1404>
2016-11-12 22:04:30 -0200

    s

---
+++ distributed_filesystem/src/main/java/distributed_filesystem/server/server/JavaServer.java
@@ -0,0 +1,46 @@
+package distributed_filesystem.server.server;
+
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.server.TThreadPoolServer;
+import org.apache.thrift.transport.TServerSocket;
+import org.apache.thrift.transport.TServerTransport;
+
+import distributed_filesystem.server.core.Calculator;
+
+public class JavaServer {
+
+  public static CalculatorHandler handler;
+
+  public static Calculator.Processor processor;
+
+  public static void main(String [] args) {
+    try {
+      handler = new CalculatorHandler();
+      processor = new Calculator.Processor(handler);
+
+      Runnable simple = new Runnable() {
+        public void run() {
+          simple(processor);
+        }
+      };      
+
+      new Thread(simple).start();
+    } catch (Exception x) {
+      x.printStackTrace();
+    }
+  }
+
+  public static void simple(Calculator.Processor processor) {
+    try {
+      TServerTransport serverTransport = new TServerSocket(9090);
+//      TServer server = new TSimpleServer(new Args(serverTransport).processor(processor));
+      
+       TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(processor));
+
+      System.out.println("Starting the simple server...");
+      server.serve();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
